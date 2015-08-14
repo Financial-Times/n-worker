74fd9ec83ba3bcf103c94bb6ab9db04c47cce0dd
Rhys Evans <rhys.evans@ft.com>
2015-08-14 14:41:25 +0100

    first implementation

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # n-worker
 Setup of basic next enhancements for non-web dynos e.g. cron-jobs, pollers
+
+## API
+
+### n-worker#setup(options)
+Sets up node enhamcemets
+ - Promise, fetch and array.find
+ - metrics for processes and fetch
+ - flags (optional)
+
+ options:
+  - `withFlags` default false - waits for the flags client to initialise and start polling
+  - `name` optional - will get the app name from package.json if absent
+  - `directory` defaults to `process.cwd()` directory in which to look for config code
+
+
+### n-worker#CronJob(options)
+Constructor for a new cron job, with metrics etc. added. `options` expects the same as [npm cron](https://www.npmjs.com/package/cron)
+
+### n-worker#metrics
+Reference to the instance of metrics used by the job
+
+
+### n-worker#flags
+Reference to the instance of flags used by the job
---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+require('array.prototype.find');
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+var flags = require('next-feature-flags-client');
+var metrics = require('next-metrics');
+var normalizeName = require('./src/normalize-name');
+var serviceMetrics = require('./src/service-metrics');
+var cron = require('cron');
+
+module.exports.setup = function (options) {
+	options = options || {};
+
+	var packageJson = {};
+
+	var defaults = {
+		withFlags: false
+	};
+
+	Object.keys(defaults).forEach(function (prop) {
+		if (typeof options[prop] === 'undefined') {
+			options[prop] = defaults[prop];
+		}
+	});
+
+	var name = options.name;
+	var description = "";
+	var directory = options.directory || process.cwd();
+
+	if (!name) {
+		try {
+			packageJson = require(directory + '/package.json');
+			name = packageJson.name;
+			description = packageJson.description || "";
+		} catch(e) {
+			// Safely ignorable error
+		}
+	}
+
+	if (!name) throw new Error("Please specify an application name");
+
+	metrics.init({ app: name, flushEvery: 40000 });
+	serviceMetrics.init();
+
+	var flagsPromise = Promise.resolve();
+
+	if (options.withFlags) {
+		flagsPromise = flags.init({ url: 'http://ft-next-feature-flags-prod.s3-website-eu-west-1.amazonaws.com/flags/__flags.json' });
+	}
+
+	return flagsPromise;
+};
+
+
+var _cronStart = cron.CronJob.prototype.start;
+cron.CronJob.prototype.start = function () {
+	Metrics.count('cron.start');
+};
+
+var _cronStop = cron.CronJob.prototype.stop;
+cron.CronJob.prototype.stop = function () {
+	Metrics.count('cron.stop');
+};
+
+module.exports.CronJob = function (opts) {
+	var _onTick = opts.onTick;
+	var _onComplete = opts.onComplete;
+
+	return new cron.CronJob({
+		cronTime: opts.cronTime,
+		start: opts.start ==== false ? true : false;,
+		timeZone: opts.timezone || 'Europe/London',
+		context: opts.context || undefined,
+		onTick: function() {
+			Metrics.count('cron.tick');
+			_onTick();
+		},
+		onComplete: function() {
+			Metrics.count('cron.success');
+			_onComplete()
+		}
+	});
+};
+
+module.exports.metrics = metrics;
+module.exports.flags = flags;
